cd01f47b531b4b79131fc3675cea1425bdbaafa4
2n2n <ayolachll@gmail.com>
2017-03-27 09:37:52 +0800

    successfully implemented jwt_authenticate

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = '2n2n'
 api = Api(app)
 
-class Student(Resource):
+jwt = JWT(app, authenticate, identity)
+
+items = []
+class Item(Resource):
+	@jwt_required()
 	def get(self, name):
-		return {'student': name}
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		return {'item': item}, 200 if item else 404
 
-api.add_resource(Student, '/student/<string:name>')
+	def post(self, name):
+		if next(filter(lambda x: x['name'] == name, items), None):
+			return {'message': 'An item with name {} alreay exists.'.format(name)}, 400
 
-app.run(port=5000)
+		data = request.get_json(force=True)
+		item = {'name': name, 'price': data['price']}
+		items.append(item)
+		return item, 201
+
+class ItemList(Resource):
+	def get(self):
+		return {'items': items }
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+app.run(port=5000, debug=True)
